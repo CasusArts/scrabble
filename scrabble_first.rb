3a9b6295c205d775c5362e44e24e3183dbb750d6
Fiodor Gorobet <casusarts@gmail.com>
2017-07-20 10:55:26 +0200

    Solvation of problem on declarative way

---
+++ scrabble_first.rb
@@ -0,0 +1,23 @@
+score_chars = {
+  "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+  "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+  "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+  "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+  "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+  "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+  "Y" => 4, "Z" => 10
+}
+
+print "Give me some word: "
+word = gets.chomp
+word.downcase!
+
+points = 0
+word.each_char do |c|
+  score_chars.each do |k, v|
+    if word[c] == k.downcase
+      points += v
+    end
+  end
+end
+puts points
